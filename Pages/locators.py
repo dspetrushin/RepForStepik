ac390bd03dd947b0ee6bd27f40d987b92f77e7d2
dspetrushin <58779075+dspetrushin@users.noreply.github.com>
2020-01-13 17:56:32 +0300

    добавление локаторов для логина и регистрации

--- Pages/locators.py
+++ Pages/locators.py
@@ -2,3 +2,11 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASS = (By.CSS_SELECTOR, "#id_login-password")
+
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASS = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_CONFIRM_PASS = (By.CSS_SELECTOR, "#id_registration-password2")
