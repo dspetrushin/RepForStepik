747636838dc4f2700da729af8bf919e6f6a8112c
dspetrushin <58779075+dspetrushin@users.noreply.github.com>
2020-01-13 17:57:54 +0300

    апдейт для задания с логином и регистрацией

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -6,3 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     page.should_be_login_link()
+
+    login_page = LoginPage(browser, browser.current_url)
+    #login_page.should_be_login_page()
+    login_page.should_be_login_form()
+    login_page.should_be_login_url()
+    login_page.should_be_register_form()
