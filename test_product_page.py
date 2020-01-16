315d9bc95dead02685e3eff0bdcd6a295c7ae0e9
dspetrushin <58779075+dspetrushin@users.noreply.github.com>
2020-01-16 12:54:11 +0300

    Обновление по 4.3_step4

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,35 @@
 import time
+import pytest
 from .Pages.product_page import ProductPage
 
 
-def test_guest_can_go_to_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+'''def test_guest_can_go_to_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
 
     page.should_be_button_add_basket()
     page.click_button_add_basket()
     page.solve_quiz_and_get_code()
+    time.sleep(10)
+    page.check_text()'''
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # ваша реализация теста
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу'''
+    page.should_be_button_add_basket()
+    page.click_button_add_basket()
+    page.solve_quiz_and_get_code()
     time.sleep(5)
+    page.check_text()
